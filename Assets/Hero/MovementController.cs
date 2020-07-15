a2c8eb428fb5019b77672659b20ac0920e5c0bf2
Daniel Doran <ddoran3@gmail.com>
2020-07-15 00:51:11 -0600

    tilemaps player movement

---
+++ Assets/Hero/MovementController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementController : MonoBehaviour {
+	new Rigidbody2D rigidbody;
+	PlayerInput input;
+	InputAction move;
+
+	public float speed = 1f;
+
+	void Start() {
+		rigidbody = GetComponent<Rigidbody2D>();
+		input = GetComponent<PlayerInput>();
+		move = input.actions["Move"];
+	}
+
+	void FixedUpdate() {
+		rigidbody.velocity = move.ReadValue<Vector2>() * speed;
+	}
+}
